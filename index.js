7bba973b8d93f997d3755e5e755b80aaff616b36
Andrew Napoleone <Naps277@gmail.com>
2023-11-12 09:12:54 -0500

    Add JS

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-import { getResistorOhms } from './resistor.js';
+import { getResistorOhms } from './resistor';
+
+function updateResistor() {
+  const color0 = document.getElementById('color0').value;
+  const color1 = document.getElementById('color1').value;
+  const color2 = document.getElementById('color2').value;
+  const color3 = document.getElementById('color3').value;
+
+  const resistor = document.getElementById('resistor');
+  resistor.style.background = `linear-gradient(to right, ${color0}, ${color1}, ${color2}, ${color3})`;
+
+  const bands = { color1, color2, multiplier: color3, tolerance: 'brown' };
+  const resistorValue = getResistorOhms(bands);
+
+  document.getElementById('resistor-value').textContent = resistorValue;
+}
+
+document.getElementById('color0').addEventListener('change', updateResistor);
+document.getElementById('color1').addEventListener('change', updateResistor);
+document.getElementById('color2').addEventListener('change', updateResistor);
+document.getElementById('color3').addEventListener('change', updateResistor);
+
+updateResistor();
+
